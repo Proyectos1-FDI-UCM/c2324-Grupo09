c774b9aa067fe054706bcd416f452fa7927122ad
Eric11196 <izandeve@ucm.es>
2024-02-15 19:48:24 +0100

    Hitbox Component Commented

--- Platformer - Project1/Assets/Scripts/CharacterController.cs
+++ Platformer - Project1/Assets/Scripts/CharacterController.cs
@@ -6,72 +6,67 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     private CharacterMovement _chMovement;
+    //reference to animation component
     private AnimationComponent _animComp;
-    //Valor que te dará el input para saber en qué dirección correr
+    //x value of the movement joystick
     int xInput;
     void Start()
     {
         _chMovement = GetComponent<CharacterMovement>();
         _animComp = GetComponentInChildren<AnimationComponent>();
     }
+
+    //Triggers on pressing the jump key
     public void JumpDown() 
     {
         _chMovement.JumpPressed();
     }
+
+    //Triggers on pressing the slide/pogo key
     public void SlideDown()
     {
         _chMovement.SlidePressed();
     }
+
+    //Triggers on changing the value of the x movement joystick
     public  void RunDown(float move)
     {
         xInput = (int) move;
 
     }
+
+    //Triggers on releasing jump button
     public void JumpUp()
     {
         _chMovement.JumpReleased();
     }
+
+    //Triggers on releasing slide button
     public void SlideUp()
     {
         _chMovement.SlideReleased();
     }
+
+    //triggers when the value of the x movement is equal to 0
     public void RunUp(float move)
     {
         xInput = (int)move;
 
     }
 
-    /*void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            _chMovement.JumpPressed();
-        }
-        else if (Input.GetButtonUp("Jump"))
-        {
-            _chMovement.JumpReleased();
-        }
 
-        if (Input.GetButtonDown("Slide"))
-        {
-            _chMovement.SlidePressed();
-        }
-        if (Input.GetButtonUp("Slide"))
-        {
-            _chMovement.SlideReleased();
-        }
-    }
-    */
-
-
+    //Runs 50 times per second
     void FixedUpdate()
     {
-      //  int input = (int)Input.GetAxisRaw("Horizontal");
+        //changes the direction the player is facing
         if(xInput != 0)
         {
             transform.localScale = new Vector3(xInput * 1, 1, 1); 
         }
+        //activates the run action of character movement
         _chMovement.Run(xInput);
+
+        //Updating animation parameters
         _animComp.UpdateXInput(xInput);
         _animComp.SetVelocityY(_chMovement.RByVel);
         _animComp.SetGrounded(_chMovement.Grounded);
--- Platformer - Project1/Assets/Scripts/HitboxComponent.cs
+++ Platformer - Project1/Assets/Scripts/HitboxComponent.cs
@@ -4,41 +4,69 @@ using UnityEngine;
 
 public class HitboxComponent : MonoBehaviour
 {
+    //reference to self transform
     private Transform _myTransform;
-    //private AttackComponent _attackComponent;
+
+    //accesor to get the value of _currentHitboxAlreadyHit from outside of this component
     public bool HitboxHit { get { return _currentHitboxAlreadyHit;  } }
 
+    //each time a hitbox is generated this is set to 0
+    //when it is set to 1 it means it hit something before the hitbox dissapeared
     private bool _currentHitboxAlreadyHit = false;
+
+    //Vector 2 containing
     [SerializeField]
     Vector2 _hitboxPosition;
+
+    //Vector 2 containing the x and y size of the box created with the hitbox
     [SerializeField]
     Vector2 _hitboxSize;
 
+    //in inspector it lets you decide which layers it is going to detect
     [SerializeField]
     private LayerMask _layerToCheck;
 
-    public void CreateHitbox(Vector2 position, Vector2 size, int relativePosition, int hitboxNumber)
+
+    /// <summary>
+    /// Turns on the hitbox and lets you decide the offset and size of the hitbox.
+    /// </summary>
+    /// <param name="position"> Offset assuming the character is looking right</param>
+    /// <param name="relativePosition"> Multiplies the position.x by -1 or 1</param>
+    /// <param name="size"> Total size of the box created</param>
+    public void CreateHitbox(Vector2 position, Vector2 size, int relativePosition)
     {
         _currentHitboxAlreadyHit = false;
-        _hitboxPosition = new Vector2(position.x * relativePosition, position.y);
-        _hitboxSize = new Vector2(size.x * relativePosition, size.y);
+        _hitboxPosition = new Vector2(position.x * Mathf.Sign(relativePosition), position.y);
+        _hitboxSize = new Vector2(size.x * Mathf.Sign(relativePosition), size.y);
     }
 
+    /// <summary>
+    /// Activates the hitbox with the values given in the inspector.
+    /// </summary>
+    public void CreateHitbox()
+    {
+        _currentHitboxAlreadyHit = false;
+        _hitboxPosition = new Vector2(_hitboxPosition.x * relativePosition, _hitboxPosition.y);
+        _hitboxSize = new Vector2(_hitboxSize.x * relativePosition, _hitboxSize.y);
+    }
+
+    ///<summary>
+    ///Disables the hitbox
+    ///Sets collision detected to 0.
+    ///</summary>
     public void DisableHitbox()
     {
         _hitboxSize = Vector2.zero;
+        _currentHitboxAlreadyHit = false;
     }
 
-    /*
-    public void CanHitAgain()
-    {
-        currentHitboxAlreadyHit = false;
-    }
-    */
 
+    /// <summary>
+    /// If this gameObject is selected on editor lets you see the size and position of the hitbox.
+    /// </summary>
     private void OnDrawGizmosSelected()
     {
-        if (!Application.isPlaying) return;
+        //if (!Application.isPlaying) return;
         Gizmos.color = Color.red;
         Gizmos.DrawCube(new Vector2(_myTransform.position.x + _hitboxPosition.x, _myTransform.position.y + _hitboxPosition.y), _hitboxSize);
     }
@@ -49,20 +77,21 @@ public class HitboxComponent : MonoBehaviour
 
     }
 
-    void CheckHit()
+    /// <summary>
+    /// Returns if Hitbox touches something of the layer indicated on the serizalize field of the hitbox component
+    /// </summary>
+    bool CheckHit()
     {
-        if 
-        (
-            Physics2D.OverlapBox(new Vector2(_myTransform.position.x + _hitboxPosition.x, _myTransform.position.y + _hitboxPosition.y), _hitboxSize, 0f, _layerToCheck)
-        )
-            _currentHitboxAlreadyHit = true;
+        return Physics2D.OverlapBox(new Vector2(_myTransform.position.x + _hitboxPosition.x, _myTransform.position.y + _hitboxPosition.y), _hitboxSize, 0f, _layerToCheck)
     }
 
+    /*
     void Update()
     {
         if (_currentHitboxAlreadyHit) return;
         CheckHit();
     }
+    */
 
 
 }
