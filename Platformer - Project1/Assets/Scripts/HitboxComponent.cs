0b6b544e6dff771c599b0a43ac1e3983146f499c
Eric11196 <izandeve@ucm.es>
2024-02-15 12:30:55 +0100

    quick refactor of hitbox component

--- Platformer - Project1/Assets/Scripts/HitboxComponent.cs
+++ Platformer - Project1/Assets/Scripts/HitboxComponent.cs
@@ -6,25 +6,35 @@ public class HitboxComponent : MonoBehaviour
 {
     private Transform _myTransform;
     //private AttackComponent _attackComponent;
-    public bool currentHitboxAlreadyHit = false;
+    public bool HitboxHit { get { return _currentHitboxAlreadyHit;  } }
+
+    private bool _currentHitboxAlreadyHit = false;
     [SerializeField]
     Vector2 _hitboxPosition;
     [SerializeField]
     Vector2 _hitboxSize;
 
     [SerializeField]
-    private LayerMask _enemyHurtboxLayer;
+    private LayerMask _layerToCheck;
 
-    public void UpdateHitbox(Vector2 position, Vector2 size, int relativePosition, int hitboxNumber)
+    public void CreateHitbox(Vector2 position, Vector2 size, int relativePosition, int hitboxNumber)
     {
+        _currentHitboxAlreadyHit = false;
         _hitboxPosition = new Vector2(position.x * relativePosition, position.y);
         _hitboxSize = new Vector2(size.x * relativePosition, size.y);
     }
 
+    public void DisableHitbox()
+    {
+        _hitboxSize = Vector2.zero;
+    }
+
+    /*
     public void CanHitAgain()
     {
         currentHitboxAlreadyHit = false;
     }
+    */
 
     private void OnDrawGizmosSelected()
     {
@@ -36,34 +46,21 @@ public class HitboxComponent : MonoBehaviour
     void Start()
     {
         _myTransform = transform;
-        //_attackComponent = GetComponent<AttackComponent>();
+
     }
 
     void CheckHit()
     {
         if 
         (
-            Physics2D.OverlapBox(new Vector2(_myTransform.position.x + _hitboxPosition.x, _myTransform.position.y + _hitboxPosition.y), _hitboxSize, 0f, _enemyHurtboxLayer)
+            Physics2D.OverlapBox(new Vector2(_myTransform.position.x + _hitboxPosition.x, _myTransform.position.y + _hitboxPosition.y), _hitboxSize, 0f, _layerToCheck)
         )
-
-        currentHitboxAlreadyHit = true;
+            _currentHitboxAlreadyHit = true;
     }
 
     void Update()
-    {   /*
-        if (_stun > 0)
-        {
-            //Debug.Log(_stun);
-            _stun--;
-            _deltaTime += Time.deltaTime;
-        }
-        else if(_stun == 0)
-        {
-            Debug.Log(_deltaTime);
-        }
-        */
-
-        if (currentHitboxAlreadyHit || _hitboxSize == Vector2.zero) return;
+    {
+        if (_currentHitboxAlreadyHit) return;
         CheckHit();
     }
 
