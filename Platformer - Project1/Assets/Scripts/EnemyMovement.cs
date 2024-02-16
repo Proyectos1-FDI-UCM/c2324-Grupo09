36db8d56f0a9d29bc3007ec380f017ebf205b4e9
DiegoAlbertoMitaChungara <dmita@ucm.es>
2024-02-16 11:47:11 +0100

    Enemy Movement

---
+++ Platformer - Project1/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region references
+    private Transform _myTransform;
+    #endregion
+    #region parameters
+    [SerializeField]
+    private float _movementSpeed = 3.0f;
+    [SerializeField]
+    private Vector3 _movementDirection = Vector3.right;
+    #endregion
+    #region methods
+    public void Direction(Vector3 direction)
+    {
+        _movementDirection = direction;
+    }
+    public void Speed(float speed)
+    {
+        _movementSpeed = speed;
+    }
+    #endregion
+    void Start()
+    {
+        _myTransform = transform;
+    }
+
+    void Update()
+    {
+        _myTransform.position += _movementSpeed * _movementDirection * Time.deltaTime;
+    }
+}
