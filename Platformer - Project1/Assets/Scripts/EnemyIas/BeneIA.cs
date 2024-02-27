a61d4f3ecf949ca0ad4d6ab9bd4ad576591c7224
DiegoAlbertoMitaChungara <dmita@ucm.es>
2024-02-27 19:39:57 +0100

    BeneIA

---
+++ Platformer - Project1/Assets/Scripts/EnemyIas/BeneIA.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeneIA : MonoBehaviour
+{
+    #region references
+    private Transform _myTransform;
+    private Transform _playerTransform;
+    private EnemyMovement _enemyMovement;
+    #endregion
+    #region parameters
+    [SerializeField]
+    private float _beneVison = 1.0f;
+    [SerializeField]
+    private float _rightOffset = 15.0f;
+    [SerializeField]
+    private float _leftOffset = 15.0f;
+    #endregion
+    void Start()
+    {
+        _myTransform = transform;
+        _playerTransform = FindObjectOfType<RefactoredCharacterMovement>().gameObject.transform;
+        _enemyMovement = GetComponent<EnemyMovement>();
+    }
+    void Update()
+    {
+        if(((Mathf.Abs(_myTransform.position.y) - Mathf.Abs(_playerTransform.position.y))<_beneVison )&& (_myTransform.position.x >= _playerTransform.position.x-_leftOffset && _myTransform.position.x <= _playerTransform.position.x+_rightOffset))
+        {
+            _enemyMovement.Direction(Vector3.down);
+        }
+    }
+
+
+}
