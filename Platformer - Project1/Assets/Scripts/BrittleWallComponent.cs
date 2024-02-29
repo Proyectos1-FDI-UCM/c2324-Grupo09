acd7892e8681238b8ad220b4db1b0a54659189df
Meldurnar <osduran@ucm.es>
2024-02-29 22:15:30 +0100

    Nuevos objetos y componentes añadidos

---
+++ Platformer - Project1/Assets/Scripts/BrittleWallComponent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class BrittleWallComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.GetComponent<Rigidbody>() != null)
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
