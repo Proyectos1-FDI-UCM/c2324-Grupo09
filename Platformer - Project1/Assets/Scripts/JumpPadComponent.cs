80c2265e12b4733440f062f8186bd6836c5aa8db
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2024-03-01 12:57:20 +0100

    Añadido indicador de trampolín

--- Platformer - Project1/Assets/Scripts/JumpPadComponent.cs
+++ Platformer - Project1/Assets/Scripts/JumpPadComponent.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpPadComponent : MonoBehaviour
 {
+    #region references
+    Transform _arrowTransform;
+    #endregion
+
     #region parameters
     [SerializeField]
     [Range(0, 7)]
@@ -11,12 +16,14 @@ public class JumpPadComponent : MonoBehaviour
     int _direction;
 
     float _angle;
-    Vector2 _JumpDirection;
+    Vector2 _jumpDirection;
     #endregion
     private void Start()
     {
+        _arrowTransform = transform.GetChild(0);
         _angle = _direction*Mathf.PI/4;
-        _JumpDirection = new Vector2(Mathf.Cos(_angle), Mathf.Sin(_angle));
+        _jumpDirection = new Vector2(Mathf.Cos(_angle), Mathf.Sin(_angle));
+        _arrowTransform.localRotation = Quaternion.Euler(0,0,_angle*180/MathF.PI-90);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
