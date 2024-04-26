ef44129979bd51cc34fc8389fb54ba56910e274d
Ian Frew <149386264+IntrovertedKilljoy@users.noreply.github.com>
2024-04-26 11:53:21 +0200

    Collider para parar la música
    Metido un collider para que pare la música a la hora de entrar en la última pantalla

---
+++ Platformer - Project1/Assets/Scripts/MusicStopper.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicStopper : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<RefactoredCharacterController>() != null)
+        {
+            try
+            {
+
+                Debug.Log("ME cago en tus muertos");
+                AudioManager.Instance.StopMusic(FMODEvents.Instance.Music);
+
+            }
+            catch
+            {
+                Debug.Log("Añadir Audio Uwu");
+            }
+
+        }
+
+    }
+}
+
